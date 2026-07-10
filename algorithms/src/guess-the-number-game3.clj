9f8b8dcafe0d670b1cd0a83ec96fd5aa1058a738
pedrofrohmut <pedrofrohmut@gmail.com>
2026-07-10 14:22:09 -0300

    Added a guess counter and a score the guess the number game

--- algorithms/src/guess-the-number-game3.clj
+++ algorithms/src/guess-the-number-game3.clj
@@ -3,6 +3,17 @@
 ;; Version 3
 ;; Guess the number game with cond and loop
 
+(defn calculate-score
+  "Calculate the game based on the number of guesses"
+  [guesses]
+
+  (let [lost-points (* guesses 10)]
+    (if (> lost-points 100)
+      0
+      (- 100 lost-points)
+
+      ))) ;; score
+
 (defn main
   "Entry point of the app"
   []
@@ -12,30 +23,39 @@
     ;; Comment it if you want it to be a secret
     (println (str "The number is: " the-number))
 
-    (loop [_ true]
+    (loop [guesses 0]
       (println "Insert a number between 1 and 100")
-      (let [input (parse-long (read-line))]
+      (let [
+            input (parse-long (read-line))
+            parser-error? (fn [n] (nil? n))
+            out-of-range-error? (fn [n] (or (< n 1) (> n 100)))
+            too-low? (fn [n] (< n the-number))
+            too-high? (fn [n] (> n the-number))
+            ]
+
         (cond
 
-          (not input)
+          (parser-error? input)
           (do (println "Parser error. Input must be a number between 1 and 100.")
-              (recur true))
+              (recur guesses))
 
-          (or (< input 1) (> input 100))
+          (out-of-range-error? input)
           (do (println "Out of range value. Value should be between 1 and 100.")
-              (recur true))
+              (recur guesses))
 
-          (< input the-number)
+          (too-low? input)
           (do (println "Too low. Try higher.")
-              (recur true))
+              (recur (inc guesses)))
 
-          (> input the-number)
+          (too-high? input)
           (do (println "Too high. Try lower.")
-              (recur true))
+              (recur (inc guesses)))
 
           :else
           (do (println "You guessed!")
-              (println (str "The number is " the-number ". Congratulations!!!")))
+              (println (str "The number is " the-number ". Congratulations!!!"))
+              (println (str "You used " (inc guesses) " to find the number."))
+              (println (str "Your score was " (calculate-score (inc guesses)) ".")))
 
           ))))) ;; main
 
